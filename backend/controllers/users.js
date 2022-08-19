1021f2e0cb74809aaa5c2cb658c073e4bb5465cf
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-08-19 15:43:01 +0300

    "fix: убрать ошибку в users"

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,3 +1,4 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -115,9 +116,7 @@ module.exports.login = (req, res, next) => {
   }
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-        { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(() => next(new UnauthorizedError('Неправильные почта или пароль')));
