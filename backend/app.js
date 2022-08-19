2b4906c5be3c398d94e7621b38adf4709053c5be
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-08-19 14:06:51 +0300

    "fix: убрать cors в app.js"

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.use('*', cors(corsConfig));
+// app.use('*', cors(corsConfig));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
